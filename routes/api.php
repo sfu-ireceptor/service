e583ebe4360c3ec2580013c737a772a5a002e6aa
Jerome Jaglale <jerome.jaglale@gmail.com>
2017-08-01 23:21:52 +0000

    Apply fixes from StyleCI

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,14 @@
 <?php
 
+use App\Lab;
+use App\Source;
+use App\DnaInfo;
+//use App\Project;
+use App\Subject;
+use App\CellSubsetType;
+use App\CaseControlType;
 use App\SampleQueryView;
 use Illuminate\Http\Request;
-use App\Lab;
-//use App\Project;
-use App\CaseControlType;
-use App\CellSubsetType;
-use App\DnaInfo;
-use App\Source;
-use App\Subject;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,62 +45,53 @@ Route::any('samples', function (Request $request) {
 });
 
 Route::any('metadata', function (Request $request) {
-		$filter = $request->all();
-		// ExternalUser::checkPermissions($filter);
-		$lab = new Lab();
-		$labprojectlist = $lab->getProjectsByLab();
-		$ethnicity= Subject::distinct()->get(array('ethnicity'));
-		$gender=Subject::distinct()->get(array('sex'));
-		$source = Source::distinct()->get(array('sample_source_name'));
-		$casecontrol = CaseControlType::distinct()->get(array('name'));
-		$dnainfo = DnaInfo::distinct()->get(array('dna_type'));
-		$cellsubsettype = CellSubsetType::get(array('subset_name'));
-		
-		
-		// instead of sending info as an object, we want to flatten it out. e.g.
-		//		gender: ["M", "F"] instead of gender: [ {"sex: M"}, {"sex: F"}]
-		// to do so, change the Eloquent query object into an indexed array - can't use
-		//		array() function, as that will change it to associative array
-		$source_array = Array();
-		$casecontrol_array = Array();
-		$dnainfo_array = Array();
-		$gender_array = Array();
-		$ethnicity_array = Array();
-		$subset_array = Array();
-		foreach ($ethnicity as $ethnicity_element)
-		{
-			$ethnicity_array[]= $ethnicity_element['ethnicity'];
-		}
-		foreach ($casecontrol as $casecontrol_element)
-		{
-			$casecontrol_array[]= $casecontrol_element['name'];
-		}
-		foreach ($dnainfo as $dnainfo_element)
-		{
-			$dnainfo_array[]= $dnainfo_element['dna_type'];
-		}
-		foreach ($source as $source_element)
-		{
-			$source_array[]= $source_element['sample_source_name'];
-		}
-		foreach ($gender as $gender_element)
-		{
-		
-			$gender_array[]= $gender_element['sex'];
-		}
-		foreach ($cellsubsettype as $subset_name)
-		{
-			$subset_array[] = $subset_name['subset_name'];
-		}
-		
-		return (json_encode(array('labs_projects'=>$labprojectlist,
-				'ethnicity'=>$ethnicity_array,
-				'casecontrol'=>$casecontrol_array,
-				'dnainfo'=>$dnainfo_array,
-				'source'=>$source_array,
-				'gender'=>$gender_array,
-				'cellsubsettypes'=>$subset_array)));
-		
+    $filter = $request->all();
+        // ExternalUser::checkPermissions($filter);
+        $lab = new Lab();
+    $labprojectlist = $lab->getProjectsByLab();
+    $ethnicity = Subject::distinct()->get(['ethnicity']);
+    $gender = Subject::distinct()->get(['sex']);
+    $source = Source::distinct()->get(['sample_source_name']);
+    $casecontrol = CaseControlType::distinct()->get(['name']);
+    $dnainfo = DnaInfo::distinct()->get(['dna_type']);
+    $cellsubsettype = CellSubsetType::get(['subset_name']);
+
+        // instead of sending info as an object, we want to flatten it out. e.g.
+        //		gender: ["M", "F"] instead of gender: [ {"sex: M"}, {"sex: F"}]
+        // to do so, change the Eloquent query object into an indexed array - can't use
+        //		array() function, as that will change it to associative array
+        $source_array = [];
+    $casecontrol_array = [];
+    $dnainfo_array = [];
+    $gender_array = [];
+    $ethnicity_array = [];
+    $subset_array = [];
+    foreach ($ethnicity as $ethnicity_element) {
+        $ethnicity_array[] = $ethnicity_element['ethnicity'];
+    }
+    foreach ($casecontrol as $casecontrol_element) {
+        $casecontrol_array[] = $casecontrol_element['name'];
+    }
+    foreach ($dnainfo as $dnainfo_element) {
+        $dnainfo_array[] = $dnainfo_element['dna_type'];
+    }
+    foreach ($source as $source_element) {
+        $source_array[] = $source_element['sample_source_name'];
+    }
+    foreach ($gender as $gender_element) {
+        $gender_array[] = $gender_element['sex'];
+    }
+    foreach ($cellsubsettype as $subset_name) {
+        $subset_array[] = $subset_name['subset_name'];
+    }
+
+    return json_encode(['labs_projects'=>$labprojectlist,
+                'ethnicity'=>$ethnicity_array,
+                'casecontrol'=>$casecontrol_array,
+                'dnainfo'=>$dnainfo_array,
+                'source'=>$source_array,
+                'gender'=>$gender_array,
+                'cellsubsettypes'=>$subset_array, ]);
 });
 /*
 |--------------------------------------------------------------------------
