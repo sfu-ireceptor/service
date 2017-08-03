3808dbcb683887929c47cc65ff3300e99b7ec450
Bojan Zimonja <bzimonja@sfu.ca>
2017-08-03 11:24:06 -0700

    added /sequences entry point

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,12 @@
 use App\Lab;
 use App\Source;
 use App\DnaInfo;
-//use App\Project;
+use App\Project;
 use App\Subject;
 use App\CellSubsetType;
 use App\CaseControlType;
 use App\SampleQueryView;
+use App\SequenceMdView;
 use Illuminate\Http\Request;
 
 /*
@@ -92,6 +93,35 @@ Route::any('metadata', function (Request $request) {
                 'source'=>$source_array,
                 'gender'=>$gender_array,
                 'cellsubsettypes'=>$subset_array, ]);
+});
+
+Route::any('sequences', function (Request $request) {
+	$filter = $request->all();
+	// ExternalUser::checkPermissions($filter);
+	
+	$t = array();
+	
+	if (!isset($filter["output"]))
+	{
+		$filter["output"] = "json";
+	}
+	switch (strtolower($filter["output"]))
+	{
+		case "csv":
+			return Response::download(SequenceMdView::createCsvGW($filter))->deleteFileAfterSend(true);
+			break;
+		case "vdjml":
+			return Response::download(SequenceMdView::createVdjml($filter))->deleteFileAfterSend(true);
+			break;	
+		default:
+			$sequence_query_list = SequenceMdView::getSequencesQuery($filter);
+			$t['items'] = $sequence_query_list;
+
+			$sequence_count = SequenceMdView::getSequencesCount($filter);
+			$t['total'] = $sequence_count;
+			return json_encode($t);
+		break;
+	}
 });
 /*
 |--------------------------------------------------------------------------
