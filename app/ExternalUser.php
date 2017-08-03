dccf624881af82b638fae403996271e20f8a0471
Jerome Jaglale <jerome.jaglale@gmail.com>
2017-08-03 18:37:18 +0000

    Apply fixes from StyleCI

--- app/ExternalUser.php
+++ app/ExternalUser.php
@@ -4,22 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ExternalUser extends Model {
-	protected $table = "external_users";
+class ExternalUser extends Model
+{
+    protected $table = 'external_users';
 
-	public static function checkPermissions($filter)
-	{
-		if ( ! isset($filter['username']))
-		{
-			app()->abort(401, 'The username parameter is required.');
-		}
+    public static function checkPermissions($filter)
+    {
+        if (! isset($filter['username'])) {
+            app()->abort(401, 'The username parameter is required.');
+        }
 
-		$username = $filter['username'];
-		$user = static::where('ireceptor_username', '=', $username)->get();
-		if ( ! isset($user[0]) || $user[0]->permission_level != 1)
-		{
-			app()->abort(401, 'The user ' . $username . ' is not authorized to access this service.');			
-		}
-		return true;
-	}
+        $username = $filter['username'];
+        $user = static::where('ireceptor_username', '=', $username)->get();
+        if (! isset($user[0]) || $user[0]->permission_level != 1) {
+            app()->abort(401, 'The user ' . $username . ' is not authorized to access this service.');
+        }
+
+        return true;
+    }
 }
