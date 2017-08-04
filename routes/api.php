a2d42e18d93e2ba3f1a4c7dba7604d62f9979164
Bojan Zimonja <bzimonja@sfu.ca>
2017-08-04 14:48:09 -0700

    -routes changed to use controllers

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,10 @@ Route::middleware('auth.basic')->group(function () {
     Route::any('metadata', 'MetadataController@index');
     Route::any('samples', 'SampleController@index');
     Route::any('sequences', 'SequenceController@index');
+    Route::any('analysis', 'SequenceController@analysis');
+    Route::any('clones', 'SequenceController@clones');
+    Route::any('sequence_summary', 'SequenceController@summary');
+    
 });
 
 /*
