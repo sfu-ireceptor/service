2746026abc3fae7e8843ffb6d01a211661ac8bd2
Bojan Zimonja <bzimonja@sfu.ca>
2017-10-17 14:14:27 -0700

    updated for AIRR terms

--- app/SampleAirrView.php
+++ app/SampleAirrView.php
@@ -14,24 +14,24 @@ class SampleAirrView extends Model
 
         $query = new self();
 
-        if (isset($f['lab_id']) && $f['lab_id'] != '') {
-            $query = $query->where('ir_lab_id', '=', $f['lab_id']);
+        if (isset($f['ir_lab_id']) && $f['ir_lab_id'] != '') {
+            $query = $query->where('ir_lab_id', '=', $f['ir_lab_id']);
         }
 
-        if (isset($f['project_id']) && ! empty($f['project_id'])) {
-            $query = $query->whereIn('ir_project_id', $f['project_id']);
+        if (isset($f['ir_project_id']) && ! empty($f['ir_project_id'])) {
+            $query = $query->whereIn('ir_project_id', $f['ir_project_id']);
         }
 
-        if (isset($f['subject_gender']) && $f['subject_gender'] != '') {
-            $query = $query->where('sex', '=', $f['subject_gender']);
+        if (isset($f['sex']) && $f['sex'] != '') {
+            $query = $query->where('sex', '=', $f['sex']);
         }
 
-        if (isset($f['subject_code']) && $f['subject_code'] != '') {
-            $query = $query->where('subject_id', 'like', '%' . $f['subject_code'] . '%');
+        if (isset($f['subject_id']) && $f['subject_id'] != '') {
+            $query = $query->where('subject_id', 'like', '%' . $f['subject_id'] . '%');
         }
 
-        if (isset($f['subject_ethnicity']) && $f['subject_ethnicity'] != '') {
-            $query = $query->where('ethnicity', '=', $f['subject_ethnicity']);
+        if (isset($f['ethnicity']) && $f['ethnicity'] != '') {
+            $query = $query->where('ethnicity', '=', $f['ethnicity']);
         }
 
         if (isset($f['subject_age_min']) && $f['subject_age_min'] != '') {
@@ -42,36 +42,36 @@ class SampleAirrView extends Model
             $query = $query->where('subject_age', '<=', $f['subject_age_max']);
         }
 
-        if (isset($f['case_control_id']) && $f['case_control_id'] != '') {
-            $query = $query->where('ir_case_control_id', '=', $f['case_control_id']);
+        if (isset($f['ir_case_control_id']) && $f['ir_case_control_id'] != '') {
+            $query = $query->where('ir_case_control_id', '=', $f['ir_case_control_id']);
         }
 
-        if (isset($f['case_control_name']) && $f['case_control_name'] != '') {
-            $query = $query->where('study_group_description', '=', $f['case_control_name']);
+        if (isset($f['study_group_description']) && $f['study_group_description'] != '') {
+            $query = $query->where('study_group_description', '=', $f['study_group_description']);
         }
 
         if (isset($f['sample_name']) && $f['sample_name'] != '') {
             $query = $query->where('sample_id', 'like', '%' . $f['sample_name'] . '%');
         }
 
-        if (isset($f['sample_source_id']) && ! empty($f['sample_source_id'])) {
-            $query = $query->whereIn('ir_sample_source_id', $f['sample_source_id']);
+        if (isset($f['ir_sample_source_id']) && ! empty($f['ir_sample_source_id'])) {
+            $query = $query->whereIn('ir_sample_source_id', $f['ir_sample_source_id']);
         }
 
-        if (isset($f['sample_source_name']) && ! empty($f['sample_source_name'])) {
-            $query = $query->whereIn('sample_type', $f['sample_source_name']);
+        if (isset($f['sample_type']) && ! empty($f['sample_type'])) {
+            $query = $query->whereIn('sample_type', $f['sample_type']);
         }
 
-        if (isset($f['dna_id']) && ! empty($f['dna_id'])) {
-            $query = $query->whereIn('ir_dna_id', $f['dna_id']);
+        if (isset($f['ir_dna_id']) && ! empty($f['ir_dna_id'])) {
+            $query = $query->whereIn('ir_dna_id', $f['ir_dna_id']);
         }
 
         if (isset($f['dna_type']) && ! empty($f['dna_type'])) {
             $query = $query->whereIn('library_source', $f['dna_type']);
         }
 
-        if (isset($f['ireceptor_cell_subset_name']) && ! empty($f['ireceptor_cell_subset_name'])) {
-            $query = $query->whereIn('cell_subset', $f['ireceptor_cell_subset_name']);
+        if (isset($f['cell_subset']) && ! empty($f['cell_subset'])) {
+            $query = $query->whereIn('cell_subset', $f['cell_subset']);
         }
 
         $list = $query->get();
