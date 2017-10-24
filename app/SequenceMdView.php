0e80d0c1e39ce048f44db991021606cb7b59d939
Bojan Zimonja <bzimonja@sfu.ca>
2017-10-24 14:43:28 -0700

    added ir_project_sample_id_list parameter

--- app/SequenceMdView.php
+++ app/SequenceMdView.php
@@ -129,10 +129,18 @@ class SequenceMdView extends Model
 
     public static function parseFilter(&$query, $f)
     {
-        $query = $query->whereIn('project_sample_id', $f['project_sample_id_list']);
+        if (isset($f['project_sample_id_list'])) {
+           $query = $query->whereIn('project_sample_id', $f['project_sample_id_list']);
+        }
+        if (isset($f['ir_project_sample_id_list'])) {
+           $query = $query->whereIn('project_sample_id', $f['ir_project_sample_id_list']);
+        }
 
         foreach ($f as $filtername => $filtervalue) {
             if (empty(self::$coltype[$filtername]) || $filtervalue == '') {
+                continue;
+            }
+            if ($filtername == 'ir_project_sample_id_list') {
                 continue;
             }
             if ($filtername == 'project_sample_id_list') {
