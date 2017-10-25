c6d3f984124bf52b65588993eb144aa40341951d
Bojan Zimonja <bzimonja@sfu.ca>
2017-10-25 13:35:20 -0700

    added ir_username parameter for v2

--- app/ExternalUser.php
+++ app/ExternalUser.php
@@ -11,11 +11,19 @@ class ExternalUser extends Model
     public static function checkPermissions($filter)
     {
         if (config('app.auth')) {
-            if (! isset($filter['username'])) {
+            if (! isset($filter['username']) && !isset($filter['ir_username'])) {
                 app()->abort(401, 'The username parameter is required.');
             }
 
-            $username = $filter['username'];
+            if (isset($filter['username']))
+            {
+              $username = $filter['username'];
+            }
+
+            if (isset($filter['ir_username']))
+            {
+              $username = $filter['ir_username'];
+            }
             $user = static::where('ireceptor_username', '=', $username)->get();
             if (! isset($user[0]) || $user[0]->permission_level != 1) {
                 app()->abort(401, 'The user ' . $username . ' is not authorized to access this service.');
