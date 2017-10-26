3dd7231c1cedb654673f2061bb930a325991125e
Bojan Zimonja <bzimonja@sfu.ca>
2017-10-26 14:49:38 -0700

    added AIRR terms to filter parameters

--- app/SampleAirrView.php
+++ app/SampleAirrView.php
@@ -25,6 +25,21 @@ class SampleAirrView extends Model
         if (isset($f['sex']) && $f['sex'] != '') {
             $query = $query->where('sex', '=', $f['sex']);
         }
+        if (isset($f['study_title']) && $f['study_title'] != '') {
+            $query = $query->where('study_title', 'like', '%' . $f['study_title'] . '%');
+        }
+
+        if (isset($f['study_description']) && $f['study_description'] != '') {
+            $query = $query->where('study_description', 'like', '%' . $f['study_description'] . '%');
+        }
+
+        if (isset($f['lab_name']) && $f['lab_name'] != '') {
+            $query = $query->where('lab_name', 'like', '%' . $f['lab_name'] . '%');
+        }
+
+        if (isset($f['organism']) && $f['organism'] != '') {
+            $query = $query->where('organism', 'like', '%' . $f['organism'] . '%');
+        }
 
         if (isset($f['subject_id']) && $f['subject_id'] != '') {
             $query = $query->where('subject_id', 'like', '%' . $f['subject_id'] . '%');
@@ -54,8 +69,24 @@ class SampleAirrView extends Model
             $query = $query->where('sample_id', 'like', '%' . $f['sample_name'] . '%');
         }
 
+        if (isset($f['disease_state_sample']) && $f['disease_state_sample'] != '') {
+            $query = $query->where('disease_state_sample', 'like', '%' . $f['disease_state_sample'] . '%');
+        }
+
+        if (isset($f['cell_phenotype']) && $f['cell_phenotype'] != '') {
+            $query = $query->where('cell_phenotype', 'like', '%' . $f['cell_phenotype'] . '%');
+        }
+
+        if (isset($f['platform']) && $f['platform'] != '') {
+            $query = $query->where('platform', 'like', '%' . $f['platform'] . '%');
+        }
+
         if (isset($f['ir_sample_source_id']) && ! empty($f['ir_sample_source_id'])) {
             $query = $query->whereIn('ir_sample_source_id', $f['ir_sample_source_id']);
+        }
+
+        if (isset($f['tissue']) && ! empty($f['tissue'])) {
+            $query = $query->whereIn('tissue', $f['tissue']);
         }
 
         if (isset($f['sample_type']) && ! empty($f['sample_type'])) {
@@ -64,6 +95,10 @@ class SampleAirrView extends Model
 
         if (isset($f['ir_dna_id']) && ! empty($f['ir_dna_id'])) {
             $query = $query->whereIn('ir_dna_id', $f['ir_dna_id']);
+        }
+
+        if (isset($f['library_source']) && ! empty($f['library_source'])) {
+            $query = $query->whereIn('library_source', $f['library_source']);
         }
 
         if (isset($f['dna_type']) && ! empty($f['dna_type'])) {
